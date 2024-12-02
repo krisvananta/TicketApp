c87472b64f19ee63448fcc05e2dac8bb9b31f95c
krisvananta <afifkrisvananta@gmail.com>
2024-12-02 16:04:36 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ticketapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.ticketapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.ticketapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+    }
+}
---
+++ app/src/main/java/com/example/ticketapp/OrderFragment.kt
@@ -0,0 +1,98 @@
+package com.example.ticketapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
+import com.example.ticketapp.databinding.FragmentOrderBinding
+import com.example.ticketapp.databinding.FragmentTicketBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [OrderFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class OrderFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentOrderBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentOrderBinding.inflate(inflater, container, false)
+        return binding.root
+        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_order, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var stringTicketType: String? = null
+
+        val stringType = resources.getStringArray(R.array.type)
+        val adapterSpinnerType = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, stringType)
+
+        binding.spinnerType.adapter = adapterSpinnerType
+        binding.spinnerType.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                stringTicketType = stringType.get(p2)
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
+
+        binding.btnBuyTicket.setOnClickListener {
+            findNavController().apply {
+                previousBackStackEntry?.savedStateHandle?.set("type", stringTicketType)
+            }.navigateUp()
+        }
+    }
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment OrderFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            OrderFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
